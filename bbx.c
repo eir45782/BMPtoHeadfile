29731cb64e16005312f1f0038867ca454072c63b
eir457882 <eir457882@gmail.com>
2020-12-29 21:43:24 +0800

    first commit

---
+++ bbx.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int main(int argc,char **argv)
+{
+
+	BITMAP_FILE_INFO_HEADER pixels;
+	FILE *fp;
+	fp = fopen("","r");
+
+	if(fp == NULL)
+	{
+		printf("bmp fucking error");
+	}
+
+
+
+
+
+
+     
